870c82a9e2d0364d3019353eec3c57e8afd665f4
OriCodes <ori.rs7@gmail.com>
2023-07-08 21:08:06 +0300

    Added functions to UserRepository

--- src/main/java/com/forums/forum/repo/UserRepository.java
+++ src/main/java/com/forums/forum/repo/UserRepository.java
@@ -1,12 +1,19 @@
 package com.forums.forum.repo;
 
+import com.forums.forum.model.Gender;
 import com.forums.forum.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
 
     boolean existsByUserName(String userName);
+    User findByUserName(String userNAme);
+
+    List<User> findAllByUserNameContaining(String userName);
+    List<User> findAllByGender(Gender gender);
 
 }
