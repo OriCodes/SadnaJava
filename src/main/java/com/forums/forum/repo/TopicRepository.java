6a5331c11df2e8a55d7bbad2806e2b0ae604fc18
OriCodes <ori.rs7@gmail.com>
2023-07-09 12:28:17 +0300

    Added TopicRepository

---
+++ src/main/java/com/forums/forum/repo/TopicRepository.java
@@ -0,0 +1,17 @@
+package com.forums.forum.repo;
+
+import com.forums.forum.model.Topic;
+import com.forums.forum.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+public interface TopicRepository extends JpaRepository<Topic,Long> {
+    boolean existsByTopicName(String topicName);
+
+    Topic findByTopicName(String topicName);
+    List<Topic> findAllByCreatedTimeStampBetween(Timestamp lowerBound, Timestamp upperBound);
+    List<Topic> findAllByTopicNameContains(String topicName);
+
+}
