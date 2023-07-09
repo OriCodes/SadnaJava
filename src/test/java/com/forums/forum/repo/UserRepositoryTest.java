396036d18ad63f2f5b32dd7b414804d042d2e1a5
OriCodes <ori.rs7@gmail.com>
2023-07-09 12:15:17 +0300

    Added tests for UserRepository

--- src/test/java/com/forums/forum/repo/UserRepositoryTest.java
+++ src/test/java/com/forums/forum/repo/UserRepositoryTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 
@@ -21,7 +23,7 @@ class UserRepositoryTest {
         underTest.deleteAll();
     }
     @Test
-    void existsByUserName() {
+    public void existsByUserName() {
         //given
         User user = new User("Poseidon", 19, "URL", Gender.MALE, "Auth");
         underTest.save(user);
@@ -34,15 +36,45 @@ class UserRepositoryTest {
     }
 
     @Test
-    void findByUserName() {
-
+    public void findByUserName()
+    {
+        //given
+        String userName = "Poseidon";
+        User user = new User(userName, 19, "URL", Gender.MALE, "Auth");
+        underTest.save(user);
+        //when
+        User foundUser = underTest.findByUserName(userName);
+        //then
+        assertThat(foundUser).isNotNull();
+        assertThat(foundUser.getUserName()).isEqualTo(userName);
     }
 
     @Test
-    void findAllByUserNameContaining() {
+    public void findAllByUserNameContaining() {
+        //given
+        User user1 = new User("Poseidon", 19, "URL", Gender.MALE, "Auth");
+        User user2 = new User("Venus", 10, "URL", Gender.FEMALE, "Auth");
+        underTest.saveAll(List.of(user1, user2));
+        String seq = "on";
+        //when
+        List<User> expected = underTest.findAllByUserNameContaining(seq);
+        //then
+        assertThat(expected.size()).isEqualTo(1);
+        assertThat(expected.get(0).getUserName().contains(seq)).isTrue();
     }
 
     @Test
-    void findAllByGender() {
+    public void findAllByGender() {
+        //given
+        User user1 = new User("Poseidon", 19, "URL", Gender.MALE, "Auth");
+        User user2 = new User("Venus", 10, "URL", Gender.FEMALE, "Auth");
+        underTest.saveAll(List.of(user1, user2));
+        Gender expectedGender = Gender.FEMALE;
+        //when
+        List<User> expectedUserList = underTest.findAllByGender(expectedGender);
+        //then
+        assertThat(expectedUserList.size()).isEqualTo(1);
+        assertThat(expectedUserList.get(0).getGender()).isEqualTo(expectedGender);
+
     }
 }
