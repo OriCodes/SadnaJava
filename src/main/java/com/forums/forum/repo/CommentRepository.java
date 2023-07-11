0e4803418c4b2839f13a65777ed20d163c6397b9
OriCodes <ori.rs7@gmail.com>
2023-07-11 21:22:26 +0300

    Added comment Repository

---
+++ src/main/java/com/forums/forum/repo/CommentRepository.java
@@ -0,0 +1,17 @@
+package com.forums.forum.repo;
+
+import com.forums.forum.model.Comment;
+import com.forums.forum.model.Post;
+import com.forums.forum.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    List<Comment> findAllByPost(Post post);
+    List<Comment> findAllByUser(User user);
+
+    int countAllByPost(Post post);
+    int countAllByUser(User user);
+}
