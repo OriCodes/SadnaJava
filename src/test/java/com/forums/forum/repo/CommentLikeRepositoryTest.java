cdb99336ec6aadd9ff4c70c27aa5370bcf47c328
OriCodes <ori.rs7@gmail.com>
2023-07-11 22:20:41 +0300

    Added CommentLikeRepositoryTest

--- src/test/java/com/forums/forum/repo/CommentLikeRepositoryTest.java
+++ src/test/java/com/forums/forum/repo/CommentLikeRepositoryTest.java
@@ -1,5 +1,7 @@
 package com.forums.forum.repo;
 
+import org.junit.jupiter.api.Test;
+
 import com.forums.forum.model.*;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,8 +15,7 @@ import java.util.List;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @DataJpaTest
-class PostLikeRepositoryTest {
-
+class CommentLikeRepositoryTest {
     @Autowired
     private UserRepository userRepository;
     @Autowired
@@ -22,51 +23,57 @@ class PostLikeRepositoryTest {
     @Autowired
     private  PostRepository postRepository;
     @Autowired
-    private PostLikeRepository postLikeRepository;
+    private CommentRepository commentRepository;
+    @Autowired
+    private CommentLikeRepository commentLikeRepository;
 
     private User user1 = new User("Poseidon", 19, "URL", Gender.MALE, "Auth");
     private User user2 = new User("Venus", 10, "URL", Gender.FEMALE, "Auth");
     private Topic topic = new Topic("Sport", new Timestamp(System.currentTimeMillis()), "URL");
-    private Post post1 = new Post("Post1", "txt", new Timestamp(System.currentTimeMillis()),user1, topic);
-    private Post post2 = new Post("Post2", "txt", new Timestamp(System.currentTimeMillis()),user2, topic);
+    private Post post = new Post("Post1", "txt", new Timestamp(System.currentTimeMillis()),user1, topic);
+    private Comment comment1 = new Comment("Text",new Timestamp(System.currentTimeMillis()),user1,post);
+    private Comment comment2 = new Comment("Text",new Timestamp(System.currentTimeMillis()),user2,post);
+
 
     @BeforeEach
     public void initiateDb(){
         userRepository.saveAll(List.of(user1, user2));
         topicRepository.save(topic);
-        postRepository.saveAll(List.of(post1,post2));
+        postRepository.save(post);
+        commentRepository.saveAll(List.of(comment1,comment2));
     }
     @AfterEach
     public void tearDown(){
-        postLikeRepository.deleteAll();
+        commentLikeRepository.deleteAll();
+        commentRepository.deleteAll();
         postRepository.deleteAll();
         topicRepository.deleteAll();
         userRepository.deleteAll();
     }
 
     @Test
-    public void countAllByPost() {
+    public void countAllByComment() {
         //given
         Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-        PostLike postLike1 = new PostLike(user1 , post1, timestamp);
-        PostLike postLike2 = new PostLike(user2 , post2, timestamp);
-        postLikeRepository.saveAll(List.of(postLike1,postLike2));
+        CommentLike commentLike1 = new CommentLike(user1,comment1,timestamp);
+        CommentLike commentLike2 = new CommentLike(user2,comment2,timestamp);
+        commentLikeRepository.saveAll(List.of(commentLike1,commentLike2));
         //when
-        int expected = postLikeRepository.countAllByPost(post1);
+        int expected = commentLikeRepository.countAllByComment(comment1);
         //then
         assertThat(expected).isEqualTo(1);
     }
 
     @Test
-    public void existsByPostAndUser() {
+    public void existsByCommentAndUser() {
         //given
         Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-        PostLike postLike1 = new PostLike(user1 , post1, timestamp);
-        PostLike postLike2 = new PostLike(user2 , post2, timestamp);
-        postLikeRepository.saveAll(List.of(postLike1,postLike2));
+        CommentLike commentLike1 = new CommentLike(user1,comment1,timestamp);
+        CommentLike commentLike2 = new CommentLike(user2,comment2,timestamp);
+        commentLikeRepository.saveAll(List.of(commentLike1,commentLike2));
         //when
-        boolean expectedTrue = postLikeRepository.existsByPostAndUser(post1,user1);
-        boolean expectedFalse = postLikeRepository.existsByPostAndUser(post2,user1);
+        boolean expectedTrue = commentLikeRepository.existsByCommentAndUser(comment1,user1);
+        boolean expectedFalse = commentLikeRepository.existsByCommentAndUser(comment1,user2);
         //then
         assertThat(expectedTrue).isTrue();
         assertThat(expectedFalse).isFalse();
