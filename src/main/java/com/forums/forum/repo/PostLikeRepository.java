8a114d352f660e2e8c674b5e46c05ba00ff1515b
OriCodes <ori.rs7@gmail.com>
2023-07-11 21:50:34 +0300

    Added PostLikeRepository

---
+++ src/main/java/com/forums/forum/repo/PostLikeRepository.java
@@ -0,0 +1,12 @@
+package com.forums.forum.repo;
+
+import com.forums.forum.model.Post;
+import com.forums.forum.model.PostLike;
+import com.forums.forum.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostLikeRepository extends JpaRepository<PostLike,Long> {
+    int countAllByPost(Post post);
+
+    boolean existsByPostAndUser(Post post, User user);
+}
