a4db4039b73f7cc20e64e9ab15b9e6272019092c
OriCodes <ori.rs7@gmail.com>
2023-07-19 16:01:47 +0300

    Added CommentLikeServiceTest

---
+++ src/test/java/com/forums/forum/service/CommentLikeServiceTest.java
@@ -0,0 +1,93 @@
+package com.forums.forum.service;
+
+import com.forums.forum.model.Comment;
+import com.forums.forum.model.CommentLike;
+import com.forums.forum.model.User;
+import com.forums.forum.repo.CommentLikeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.verify;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@ExtendWith(MockitoExtension.class)
+class CommentLikeServiceTest {
+    @Mock
+    private CommentLikeRepository commentLikeRepository;
+    private CommentLikeService commentLikeService;
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+        commentLikeService = new CommentLikeService(commentLikeRepository);
+    }
+
+    @Test
+    public void getNumberOfLikes() {
+        //given
+        Comment comment = new Comment();
+        //then
+        commentLikeService.getNumberOfLikes(comment);
+        //when
+        ArgumentCaptor<Comment> commentArgumentCaptor =  ArgumentCaptor.forClass(Comment.class);
+        verify(commentLikeRepository).countAllByComment(commentArgumentCaptor.capture());
+        Comment capturedComment = commentArgumentCaptor.getValue();
+        assertThat(capturedComment).isEqualTo(comment);
+
+    }
+
+    @Test
+    public void hasLiked() {
+        //given
+        Comment comment = new Comment();
+        User user = new User();
+        //then
+        commentLikeService.hasLiked(user,comment);
+        //when
+        ArgumentCaptor<Comment> commentArgumentCaptor =  ArgumentCaptor.forClass(Comment.class);
+        ArgumentCaptor<User> userArgumentCaptor =  ArgumentCaptor.forClass(User.class);
+        verify(commentLikeRepository).existsByCommentAndUser(commentArgumentCaptor.capture(),userArgumentCaptor.capture());
+
+        Comment capturedComment = commentArgumentCaptor.getValue();
+        User capturedUser = userArgumentCaptor.getValue();
+        assertThat(capturedComment).isEqualTo(comment);
+        assertThat(capturedUser).isEqualTo(user);
+    }
+
+    @Test
+    public void deleteLike() {
+        //given
+        Comment comment = new Comment();
+        User user = new User();
+        //then
+        commentLikeService.deleteLike(comment,user);
+        //when
+        ArgumentCaptor<Comment> commentArgumentCaptor =  ArgumentCaptor.forClass(Comment.class);
+        ArgumentCaptor<User> userArgumentCaptor =  ArgumentCaptor.forClass(User.class);
+        verify(commentLikeRepository).deleteAllByCommentAndUser(commentArgumentCaptor.capture(),userArgumentCaptor.capture());
+
+        Comment capturedComment = commentArgumentCaptor.getValue();
+        User capturedUser = userArgumentCaptor.getValue();
+        assertThat(capturedComment).isEqualTo(comment);
+        assertThat(capturedUser).isEqualTo(user);
+    }
+
+    @Test
+    public void addLike() {
+        //given
+        CommentLike commentLike = new CommentLike();
+        //then
+        commentLikeService.addLike(commentLike);
+        //when
+        ArgumentCaptor<CommentLike> commentLikeArgumentCaptor =  ArgumentCaptor.forClass(CommentLike.class);
+        verify(commentLikeRepository).save(commentLikeArgumentCaptor.capture());
+
+        CommentLike capturedCommentLike = commentLikeArgumentCaptor.getValue();
+        assertThat(capturedCommentLike).isEqualTo(commentLike);
+    }
+}
