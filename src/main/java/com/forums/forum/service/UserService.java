e9832f98fb8fbe76ad1d1559331bcd097c95e174
OriCodes <ori.rs7@gmail.com>
2023-07-19 17:20:09 +0300

    Added addUser

--- src/main/java/com/forums/forum/service/UserService.java
+++ src/main/java/com/forums/forum/service/UserService.java
@@ -1,5 +1,8 @@
 package com.forums.forum.service;
 
+import com.forums.forum.exception.UserNameAlreadyExistException;
+import com.forums.forum.model.Gender;
+import com.forums.forum.model.User;
 import com.forums.forum.repo.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,5 +11,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService {
     private final UserRepository userRepository;
-    
+
+    public User addUser(String userName, Integer age, String profileUrl, Gender gender, String auth0Id) throws UserNameAlreadyExistException
+    {
+        if(userRepository.existsByUserName(userName)){
+            throw new UserNameAlreadyExistException();
+        }
+        User newUser = new User(userName, age,  profileUrl, gender,  auth0Id);
+        return userRepository.save(newUser);
+    }
+
 }
