7829bcf5a747f29b5e43b90b567dacb154eb2da5
Amit Nave <amitnave@nucleaimd.com>
2023-08-19 20:20:48 +0300

    started authentication

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,15 @@
 import ErrorPage from "@/components/ErrorPage";
 import PageLayout from "@/components/PageLayout";
 import TopicPage from "@/components/Topic/TopicPage";
+import { Auth0Provider } from "@auth0/auth0-react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+declare const authConfig: {
+  VITE_AUTH0_DOMAIN: string;
+  VITE_AUTH0_CLIENT_ID: string;
+};
 
 function App() {
   const router = createBrowserRouter([
@@ -19,6 +25,7 @@ function App() {
       ],
     },
   ]);
+
   const client = new QueryClient({
     defaultOptions: {
       queries: {
@@ -28,11 +35,19 @@ function App() {
   });
 
   return (
-    <QueryClientProvider client={client}>
-      <ChakraProvider>
-        <RouterProvider router={router}></RouterProvider>
-      </ChakraProvider>
-    </QueryClientProvider>
+    <Auth0Provider
+      domain={authConfig.VITE_AUTH0_DOMAIN}
+      clientId={authConfig.VITE_AUTH0_CLIENT_ID}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <QueryClientProvider client={client}>
+        <ChakraProvider>
+          <RouterProvider router={router}></RouterProvider>
+        </ChakraProvider>
+      </QueryClientProvider>
+    </Auth0Provider>
   );
 }
 
--- frontend/src/components/PageLayout.tsx
+++ frontend/src/components/PageLayout.tsx
@@ -1,16 +1,23 @@
-import { Box, Flex, Heading } from "@chakra-ui/react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { Box, Button, Flex, Heading } from "@chakra-ui/react";
 import { FunctionComponent } from "react";
 import { Outlet } from "react-router-dom";
 
 interface PageLayoutProps {}
 
 const PageLayout: FunctionComponent<PageLayoutProps> = () => {
+  const { isAuthenticated, loginWithPopup } = useAuth0();
   return (
     <Box p={4}>
       <Flex justifyContent="space-between" alignItems="center">
         <Heading as="h1" size="lg">
-          Forum Sadna
+          TALKSphere
         </Heading>
+        {isAuthenticated ? (
+          <p>Logged in</p>
+        ) : (
+          <Button onClick={() => loginWithPopup()}>Login</Button>
+        )}
       </Flex>
       <Box>
         <Outlet />
