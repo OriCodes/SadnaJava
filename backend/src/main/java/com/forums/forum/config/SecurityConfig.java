ca096f528a75828a5bc983ecb7cf47623318f601
OriCodes <ori.rs7@gmail.com>
2023-08-20 19:02:56 +0300

    Edited to work with google-login

--- backend/src/main/java/com/forums/forum/config/SecurityConfig.java
+++ backend/src/main/java/com/forums/forum/config/SecurityConfig.java
@@ -1,15 +1,20 @@
 package com.forums.forum.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+
 
 @EnableWebSecurity
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http.oauth2Login()
-                .and().build();
+      return http
+              .formLogin(Customizer.withDefaults())
+              .oauth2Login(Customizer.withDefaults())
+              .authorizeHttpRequests(c->c.anyRequest())
+              .build();
     }
 }
