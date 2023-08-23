42c73120c1ea515a0e23741a640ed2872600b01b
Amit Nave <amitnave@nucleaimd.com>
2023-08-24 01:41:21 +0300

    added register button

--- frontend/src/components/PageLayout.tsx
+++ frontend/src/components/PageLayout.tsx
@@ -1,3 +1,4 @@
+import useAuthStore from "@/store/auth";
 import { Box, Button, Flex, Heading } from "@chakra-ui/react";
 import { FunctionComponent } from "react";
 import { Link, Outlet } from "react-router-dom";
@@ -5,6 +6,8 @@ import { Link, Outlet } from "react-router-dom";
 interface PageLayoutProps {}
 
 const PageLayout: FunctionComponent<PageLayoutProps> = () => {
+  const { isLoggedIn } = useAuthStore();
+
   return (
     <Box p={4}>
       <Flex justifyContent="space-between" alignItems="center">
@@ -13,9 +16,27 @@ const PageLayout: FunctionComponent<PageLayoutProps> = () => {
             <img src="assets/svg/TALKSphere.svg" />
           </Heading>
         </Link>
-        <Link to="/login">
-          <Button>Log in</Button>
-        </Link>
+        <Flex>
+          {isLoggedIn ? (
+            <Button colorScheme="teal" as={Link} to="/login">
+              Logout
+            </Button>
+          ) : (
+            <>
+              <Button colorScheme="teal" as={Link} to="/login">
+                Login
+              </Button>
+              <Button
+                colorScheme="teal"
+                as={Link}
+                to="/register"
+                marginLeft={2}
+              >
+                Register
+              </Button>
+            </>
+          )}
+        </Flex>
       </Flex>
       <Box>
         <Outlet />
