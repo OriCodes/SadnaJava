f29f2c35beaeaa471aa9983033348c9522f3d41b
OriCodes <ori.rs7@gmail.com>
2023-08-27 20:31:35 +0300

    Fixed bug that caused throwing bad status

--- backend/src/main/java/com/forums/forum/auth/CustomExceptionHandler.java
+++ backend/src/main/java/com/forums/forum/auth/CustomExceptionHandler.java
@@ -1,5 +1,8 @@
 package com.forums.forum.auth;
 
+import com.forums.forum.exception.ResourceNotFoundException;
+import com.forums.forum.exception.TopicAlreadyExistException;
+import com.forums.forum.exception.UserActionNotAllowedException;
 import com.forums.forum.exception.UserNameAlreadyExistException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,8 +14,19 @@ public class CustomExceptionHandler {
 
     @ExceptionHandler(UserNameAlreadyExistException.class)
     @ResponseStatus(HttpStatus.CONFLICT)
-    public void handleUserNameAlreadyExistException() {
-        // No need to add any code here, just the annotation is enough
-    }
+    public void handleUserNameAlreadyExistException() {}
+
+    @ExceptionHandler(UserActionNotAllowedException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public void handleUserActionNotAllowedException() {}
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public void handleResourceNotFoundException() {}
+
+    @ExceptionHandler(TopicAlreadyExistException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public void TopicAlreadyExistException() {}
+
 }
 
