354f37132a9f257494a243d4b21ff3577b4efbdc
Amit Nave <amitnave@nucleaimd.com>
2023-08-31 21:14:22 +0300

    removed sidebar for users not logged in

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,25 +1,35 @@
+import useAuthStore from "@/store/auth";
 import { Box, Button, Spacer, VStack } from "@chakra-ui/react";
 import { Link } from "react-router-dom"; // Assuming you're using react-router for routing
 
 const Sidebar = () => {
+  const authStore = useAuthStore();
+
+  const { isLoggedIn } = authStore;
+
   return (
     <Box w="250px" h="100vh" p="4">
       <VStack spacing="4" align="stretch">
         <Button as={Link} to="/" variant="ghost" size="lg" fontWeight="normal">
           Home
         </Button>
-        <Button as={Link} to="/create-post" colorScheme="teal" size="lg">
-          Create New Post
-        </Button>
-        <Button
-          as={Link}
-          to="/messages"
-          variant="ghost"
-          size="lg"
-          fontWeight="normal"
-        >
-          Messages
-        </Button>
+
+        {isLoggedIn && (
+          <>
+            <Button as={Link} to="/create-post" colorScheme="teal" size="lg">
+              Create New Post
+            </Button>
+            <Button
+              as={Link}
+              to="/messages"
+              variant="ghost"
+              size="lg"
+              fontWeight="normal"
+            >
+              Messages
+            </Button>
+          </>
+        )}
       </VStack>
       <Spacer />
     </Box>
